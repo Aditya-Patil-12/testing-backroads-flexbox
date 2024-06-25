70bc44341621ebb9da98acbef20ef7c9745e16f5
Aditya-Patil-12 <aditypatil71@gmail.com>
2024-06-25 14:14:34 +0530

    intial commit

---
+++ JavaScript/n.js
@@ -0,0 +1,26 @@
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+console.log(links);
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 74.85;
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
